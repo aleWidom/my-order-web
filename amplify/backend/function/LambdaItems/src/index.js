1dec0a2415da39bd016b3e28a2ac54c95a14a03f
aleWidomlanski <widomlanski@hotmail.com>
2023-09-17 15:03:55 -0300

    lambdas update

--- amplify/backend/function/LambdaItems/src/index.js
+++ amplify/backend/function/LambdaItems/src/index.js
@@ -1,17 +1,73 @@
-
-
 /**
+ *
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
+
+
 exports.handler = async (event) => {
-    console.log(`EVENT: ${JSON.stringify(event)}`);
+
+    let mysql = require('mysql');
+
+    let connection;
+
+    let result;
+
+    
+ try {
+     
+     connection= mysql.createConnection({
+        host: 'myorderdatabase.cluster-ctulrcqrkejd.us-east-1.rds.amazonaws.com',
+        user: 'admin',
+        password: 'admin123456',
+        database: 'myorder'
+     })
+     
+    let queryMySql = `select * from Item`
+    
+    if (event.queryStringParameters?.categories !== undefined) {
+        queryMySql = `select * from Category`
+    }
+    else if (event.queryStringParameters?.search !== undefined) {
+        queryMySql = `select * from Item where title like "%${event.queryStringParameters.search}%"`
+    }
+    else if (event.queryStringParameters?.searchCard !== undefined) {
+        queryMySql = `select * from Item where title = "%${event.queryStringParameters.search}%"`
+    }
+    else if (event.queryStringParameters?.ranking !== undefined) {
+        queryMySql = `SELECT * FROM Item WHERE id IN(2,61,83)`
+    }
+    else if (event.queryStringParameters?.dayPlates!== undefined) {
+        queryMySql = `SELECT * FROM Item WHERE id IN(37,85,81,111)`
+    }
+    else if (event.queryStringParameters?.specials!== undefined) {
+        queryMySql = `SELECT * FROM Item WHERE id IN(47,72,89)`
+    }
+    
+   
+
+
+    const promiseQuery = new Promise((resolve) => {
+        connection.query(`${queryMySql}`, function (error, results, fields) {
+            resolve(results)
+        });
+    })
+    result = await promiseQuery
+}
+  catch (err) {
+    return err
+  } finally {
+    if (connection) await connection.end() 
+  }
+
+
+
+
     return {
         statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  },
-        body: JSON.stringify('Hello from Lambda!'),
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "*"
+        },
+        body: JSON.stringify(result),
     };
 };
--- amplify/backend/function/LambdaTables/src/index.js
+++ amplify/backend/function/LambdaTables/src/index.js
@@ -1,17 +1,83 @@
-
-
 /**
+ *
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
+
+
 exports.handler = async (event) => {
-    console.log(`EVENT: ${JSON.stringify(event)}`);
+
+    let mysql = require('mysql');
+
+    let connection;
+
+    let result;
+
+    
+ try {
+     
+     connection= mysql.createConnection({
+        host: 'myorderdatabase.cluster-ctulrcqrkejd.us-east-1.rds.amazonaws.com',
+        user: 'admin',
+        password: 'admin123456',
+        database: 'myorder'
+     })
+     
+    let queryMySql = `select * from Table_`
+    
+    if (event.queryStringParameters?.active !== undefined) {
+        queryMySql = `select * from Table_ Where table_active = 1`
+    }
+    else if (event.queryStringParameters?.activeCall !== undefined) {
+        queryMySql = `select * from Table_ Where table_call = 1`
+    }
+    else if (event.queryStringParameters?.searchTable !== undefined) {
+       queryMySql = `select * from Table_ Where table_number = ${event.pathParameters.proxy}`
+    }
+    else if (event.queryStringParameters?.activate !== undefined) {
+        if (event.queryStringParameters.activate === "activate") {
+            queryMySql = `UPDATE Table_ Set table_active = 1 Where table_number = ${event.pathParameters.proxy}`
+        }
+    }
+    else if (event.queryStringParameters?.desactivate !== undefined) {
+        if (event.queryStringParameters?.desactivate === "desactivate") {
+            queryMySql = `UPDATE Table_ Set table_active=0, table_call=0  Where table_number = ${event.pathParameters.proxy}`
+        }
+    }
+    else if (event.queryStringParameters?.call !== undefined) {
+        if (event.queryStringParameters?.call === "call") {
+           queryMySql = `UPDATE Table_ Set table_call= 1 Where table_number = ${event.pathParameters.proxy}`
+        }
+        else if (event.queryStringParameters?.call === "notCall") {
+           queryMySql = `UPDATE Table_ Set table_call= 0 Where table_number = ${event.pathParameters.proxy}`
+        }
+        else if (event.queryStringParameters?.ordersCreate !== undefined) {
+           queryMySql = `INSERT INTO Order_ (id_table) VALUES (${event.pathParameters.proxy});`
+        }
+    }
+
+
+    const promiseQuery = new Promise((resolve) => {
+        connection.query(`${queryMySql}`, function (error, results, fields) {
+            resolve(results)
+        });
+    })
+    result = await promiseQuery
+}
+  catch (err) {
+    return err
+  } finally {
+    if (connection) await connection.end() 
+  }
+
+
+
+
     return {
         statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  },
-        body: JSON.stringify('Hello from Lambda!'),
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "*"
+        },
+        body: JSON.stringify(result),
     };
 };
