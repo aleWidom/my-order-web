c88035f17ba29e15502eadcd7371e697b866fe7a
aleWidomlanski <widomlanski@hotmail.com>
2023-09-19 20:20:08 -0300

    update cors

--- amplify/backend/function/LambdaItems/src/index.js
+++ amplify/backend/function/LambdaItems/src/index.js
@@ -65,8 +65,8 @@ exports.handler = async (event) => {
     return {
         statusCode: 200,
         headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Headers": "*"
+           /* "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "*" */
         },
         body: JSON.stringify(result),
     };
