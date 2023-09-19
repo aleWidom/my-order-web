9a2a8b5265810f947bd79cf86a4e627b4a45a73b
aleWidomlanski <widomlanski@hotmail.com>
2023-09-19 20:25:51 -0300

    cors ok

--- amplify/backend/function/LambdaTables/src/index.js
+++ amplify/backend/function/LambdaTables/src/index.js
@@ -75,8 +75,8 @@ exports.handler = async (event) => {
     return {
         statusCode: 200,
         headers: {
-            /* "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Headers": "*" */
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "*"
         },
         body: JSON.stringify(result),
     };
