b5247e79535a871556dc9fb4da406dda7b0b1d33
alejandro <widomlanski@hotmail.com>
2025-02-04 15:51:53 -0300

    link nuevo api recuperacion

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,31 +1,12 @@
 export type AmplifyDependentResourcesAttributes = {
   "api": {
-    "myorder": {
-      "ApiId": "string",
-      "ApiName": "string",
-      "RootUrl": "string"
-    },
-    "myorderapi": {
+    "myorderwebrecuperadoapi": {
       "ApiId": "string",
       "ApiName": "string",
       "RootUrl": "string"
     }
   },
   "function": {
-    "LambdaItems": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "LambdaExecutionRoleArn": "string",
-      "Name": "string",
-      "Region": "string"
-    },
-    "LambdaTables": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "LambdaExecutionRoleArn": "string",
-      "Name": "string",
-      "Region": "string"
-    },
     "items": {
       "Arn": "string",
       "LambdaExecutionRole": "string",
